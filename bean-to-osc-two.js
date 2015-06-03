adcf79c7bb23b19131b406637774a9fe0c374195
Nadine Lessio <idoru00@gmail.com>
2015-06-03 19:33:12 -0400

    updated parse and write data

--- bean-to-osc-two.js
+++ bean-to-osc-two.js
@@ -4,6 +4,8 @@
 // additions: passing the bean name, enbale only if BT is active, added a listening socket. 
 // Alex = changes to a few functions. 
 
+// ISSUES: line 69 - 108, how to write back to a scratch bank?
+
 "use strict";
 Buffer.prototype.toByteArray = function() { return Array.prototype.slice.call(this, 0); }; // not sure what is going on here...
 
@@ -17,7 +19,9 @@ var scratchOne = "a495ff21c5b14b44b5121370f02d74de",
 // ******
 // Adjust based on however many scratch characteristics you need to read. 
 // Up to the three listed above. (Comma separated within brackets below.)
+
 var scratch = [scratchOne];
+var serviceUUID = 'a495ff10c5b14b44b5121370f02d74de'; // look for bean specific characteristics
 
 // ******
 
@@ -53,33 +57,15 @@ console.log("OSC listener running at http://localhost:" + inport);
 // OSC Talk back from Processing
 // I still feel like this is wrong. 
 
-
 var sock = dgram.createSocket("udp4", onCreateSocket);
-
 function onCreateSocket(msg, rinfo){
-
-	var isAll;
+	
 	isAll = parseMsg(msg);
 	writeData(isAll);
+};
 
-	function writeData() {
+sock.bind(inport); 
 
-		b = beanArray; //yeah i know. 
-		
-		//ok this is AT LEAST getting both bean objects into this thing. So I have something to work with
-
-		console.log("!writeData", b.length); 
-		console.log("writeData", isAll);
-
-		var colour = isAll[0];
-		var whichBean = isAll[1];
-		console.log("writeData: ", colour, whichBean);
-
-		// crap...I need to figure out how to set up a write back to a scratch bank.
-
-	}
-	
-}
 
 function parseMsg(msg) {
 	//console.log("!parseMsg",msg);
@@ -101,15 +87,47 @@ function parseMsg(msg) {
         isAll.push(isMsg);
         isAll.push(isAdd);
         isAll.push(isType);
-
-        // return the array 
-        //console.log(isAll);
         return isAll; 
-	
-}
+};
 
-sock.bind(inport); 
+/// This is where I'm having the issue. Basically, writeData and writeBeanCharacteristic have to be the same thing. BUT...I have to get name and 
+// characteristics from SubscribeToChars down the chain there, and isAll from the socket. How the hell do I do this? 
 
+var isAll;
+
+function writeData() {
+
+		console.log("writeData", isAll);
+
+		var colour = isAll[0];
+		var whichBean = isAll[1];
+		console.log("writeData: ", colour, whichBean);
+
+		// crap...I need to figure out how to either get WriteBeanCharateristics in here or get writeData into writeBeanCharacteristics
+
+	};
+
+
+
+
+function writeBeanCharacteristic(name, characteristics) {
+
+	characteristics.forEach(function (nobleObject, index) {
+
+		//console.log("!writeForEach: ", nobleObject, "idx: ", index)
+		var scratchNumber = index + 2;
+		console.log("!writeForEach: ", characteristics);
+
+		// not even sure if this is right...
+
+		
+		nobleObject.notify(true, function(err) {
+			if (err) throw err;
+		});
+
+	});
+
+ };
 
 
 // Send data over OSC to Processing
@@ -139,12 +157,10 @@ var sendDataToOSC = null;
 		oscBuffer = null;
 
 	};
-
-
 };
 
 
-var serviceUUID = 'a495ff10c5b14b44b5121370f02d74de'; // look for bean specific characteristics
+
 
 var subscribeToChars = function(name, characteristics) {
 
@@ -154,7 +170,6 @@ var subscribeToChars = function(name, characteristics) {
 
 	characteristics.forEach(function (nobleObject, index) {
 
-		//console.log('whereIsThisBreaking', nobleObject, 'idx', index);
 
 		var scratchNumber = index + 1;
 
@@ -177,14 +192,20 @@ var subscribeToChars = function(name, characteristics) {
 };
 
 var setupChars = function(peripheral) {
+
 	var name = peripheral.advertisement.localName;
 
 	console.log('insideSetupChars!', name);
 
 	peripheral.discoverSomeServicesAndCharacteristics([],scratch,function(err,services,characteristics) {
 		if (err) throw err;
+
 		console.log('SetupChars!', name);
-		subscribeToChars(name, characteristics);
+
+		subscribeToChars(name, characteristics); // pass to subscribe / read
+
+		writeBeanCharacteristic(name, characteristics); // pass to write function
+
 		
 	});
 
@@ -204,7 +225,6 @@ var setupPeripheral = function(a) {
 
         a.on('disconnect', function(){
 	    	console.log(a.advertisement.localName + " bean disconnected, trying to find it...");
-
         	noble.startScanning();
 	    });
 
@@ -213,6 +233,7 @@ var setupPeripheral = function(a) {
 }; 
 
 var beanArray = [];
+var maxLength = 4;
 
 
 noble.on('discover', function(peripheral) {
@@ -223,21 +244,24 @@ noble.on('discover', function(peripheral) {
 	  if (_.contains(peripheral.advertisement.serviceUuids, serviceUUID)) {
 	  	console.log("Found a Bean");
 
-	  	//noble.stopScanning();
-	    //console.log("Stopped scanning.");
-	    // Once found, connect:
+	  	// stop looking for beans when you've connected to 4 of them. 
+
+	  	if (beanArray.length >= 4) {
+	  		noble.stopScanning();
+	    	console.log("4 beans are connected. Stopped scanning.");
+	  	}
+	  	
+	    // Once found, puch the array through to "peripheral"
+	    
 	    beanArray.push(peripheral);
+
+	    // throw them all to the setup function
 	  	setupPeripheral(peripheral);
 
 	  } else {
 	  	console.log("Found a random BLE device, that is not a Bean, ignored.");
 	  }
 
-
-	//console.log("beanArray:", beanArray.length, beanArray[0], beanArray[1]);
-	if(beanArray.length > 2 ){
-		beanConnect(beanArray);
-	}
 
 });
 
@@ -264,67 +288,5 @@ process.on('SIGINT', exitHandler.bind(null, {exit:true}));
 
 //catches uncaught exceptions
 process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
-
-
-/// Just saving this ////
-
-/*var writeToChars = function (nobleObject, name, characteristics) { // this is passing values from the BLE setup function
-
-	console.log("writeToChars", nobleObject, name, characteristics);
-
-	// i need to get isAll to here. 
-	// eventually this will write some values from isAll into a scratch bank. 
-
-}*/
-/*var sock = dgram.createSocket("udp4", function(msg, rinfo, isAll) {
-	
-    try {
-        
-        // get at all that info being barffed out at you from Processing.
-        
-        //console.log(osc.fromBuffer(msg));
-
-        var getMsg = osc.fromBuffer(msg);
-        var isMsg = getMsg.args[0].value;
-        var isName = getMsg.args[1].value;
-        var isAdd = getMsg.address;
-        var isType = getMsg.oscType;
-
-        // make an array out of it
-
-        var isAll = [];
-        isAll.push(isName);
-        isAll.push(isMsg);
-        isAll.push(isAdd);
-        isAll.push(isType);
-
-        // return the array 
-
-        return isAll; 
-        //writeToChars(isAll);
-        //passMsg(isAll);
-      
-
-    } catch (error) {
-        //console.log("invalid OSC packet");
-        console.log(error);
-    }
-});*/
-
-
-/*function passMsg (isAll) {
-	var b = isAll;
-	return b;
-
-	// for now just see if you can write it to the scratch bank.
-
-	// if is address == X type == message
-		//  if isName == name
-			// write msg to scratch bank two
-
-			// if isname == other name 
-				// write msg to scratch bank two  
-}*/
-
 
 
